4f73749c93bb2ab88a938c3095d9f92e5e2e9333
João Pedro Cruz <joaopedrocruz.15@hotmail.com>
2025-08-06 16:04:25 -0300

    Iniciado projeto e-commerce com Laravel12

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::prefix('admin')->group(function (){
+    //Show login form
+    Route::get('login', [AdminController::class, 'create'])->name('admin.login');
+
+    //Handle login form submission
+    Route::post('login', [AdminController::class, 'store'])->name('admin.login.request');
+
+    Route::group(['middleware' => ['admin']], function (){
+
+        // Dashboard route
+        Route::resource('dashboard', AdminController::class)->only(['index']);
+
+        // Admin logout
+        Route::get('logout', [AdminController::class, 'destroy'])->name('admin.logout');
+
+    });
+});
+
